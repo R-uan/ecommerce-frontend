dd56efbd5a924e6767125af0069b10334865a8de
Ruan <ruan.oliveiralopes@hotmail.com>
2024-02-19 07:24:52 -0300

    catalog tweaks

--- src/app/(catalog)/components/ProductMiniature.tsx
+++ src/app/(catalog)/components/ProductMiniature.tsx
@@ -1,22 +1,25 @@
 import styles from "@/styles/product-mini.module.scss";
 import Image from "next/image";
-import testimage from "@/public/testimage.jpg";
+import testimage from "../../../../public/images/testvertical.jpg";
 import IProducts from "@/interfaces/IProducts";
 export default function ProductMiniature({ data }: { data: IProducts }) {
+	const { availability } = data;
 	return (
 		<div className={styles.main_container}>
 			<div className={styles.product_image}>
-				<Image src={testimage} alt=""></Image>
+				{!availability ? null : (
+					<div className={styles.unavailable_item}>
+						<h3>NOT AVAILABLE</h3>
+					</div>
+				)}
+				<Image className={styles.image} src={testimage} alt=""></Image>
 			</div>
 			<div className={styles.product_information}>
 				<div className={styles.info1}>
 					<h3>{data.name}</h3>
-					<span>Rating</span>
+					<h6>{data.manufacturer_name}</h6>
 				</div>
 				<div className={styles.info2}>
-					<h3>{data.manufacturer_name}</h3>
-				</div>
-				<div className={styles.info3}>
 					<h3>{data.category}</h3>
 					<span>${data.unit_price}</span>
 				</div>
