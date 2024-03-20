896ffe8aad3488c2f54c168c46a6e5d41a47b84b
Ruan <ruan.oliveiralopes@hotmail.com>
2024-03-20 13:23:29 -0300

    request class

--- src/scripts/requests/RequestProducts.ts
+++ src/scripts/requests/RequestProducts.ts
@@ -1,87 +1,85 @@
-import axios, { AxiosError, isAxiosError } from "axios";
-import ProductFetchingError from "../error-handling/ProductFetchingError";
-import UnexpectedError from "../error-handling/UnexpectedError";
-import { ProductQuery } from "@/interfaces/ProductQuery";
-import api from "./axios-instances/PublicAxiosInstance";
-config();
-import { config } from "dotenv";
-import { IPlanetDestination } from "@/interfaces/IOrder";
+import Requests from "./Requests";
+import { isAxiosError } from "axios";
 import { ICart } from "@/interfaces/ICart";
+import { IProduct } from "@/interfaces/IProduct";
+import api from "./axios-instances/PublicAxiosInstance";
+import { IPlanetDestination } from "@/interfaces/IOrder";
+import { ProductQuery } from "@/interfaces/ProductQuery";
+import IPaginateResponse from "@/interfaces/IPaginateResponse";
+import ApiRequestError from "../error-handling/APIRequestError";
+import UnexpectedError from "../error-handling/UnexpectedError";
 
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL as string;
-
-export async function RequestProducts(url: string = `${BACKEND_URL}/products/miniatures`) {
-	try {
-		const response = (await axios.get(url)).data;
-		return {
-			data: response.data,
-			total: response.total,
-			current_page: response.current_page,
-			last_page: response.last_page,
-			next_page_url: response.next_page_url || "end",
-		};
-	} catch (error) {
-		console.log(error);
-		if (isAxiosError(error)) throw new Error(error.message);
-		else throw new Error("Unexpected Error.");
-	}
-}
-
-export async function RequestProductsQuery(params: string, url?: string, query: ProductQuery = ProductQuery.Name) {
-	let response;
-	if (url) response = (await axios.get(url)).data;
-	else response = (await axios.get(`${BACKEND_URL}/products/search?${query}[lk]=${params}`)).data;
-
-	return {
-		data: response.data,
-		total: response.total,
-		current_page: response.current_page,
-		last_page: response.last_page,
-		next_page_url: response.next_page_url,
-		last_page_url: response.last_page_url,
-	};
-}
-
-export async function RequestSingleProduct(id: string) {
-	try {
-		const response: IProduct = (await axios.get(`${BACKEND_URL}/products/${id}`)).data;
-		return response;
-	} catch (error) {
-		if (error instanceof AxiosError) {
-			if (error.response?.status == 404) {
-				throw new ProductFetchingError(error.response.data.message);
-			}
-		} else throw new UnexpectedError();
-	}
-}
-
-export async function RequestFromManufacturer(id: number) {
-	try {
-		const response = (await axios.get(`${BACKEND_URL}/manufacturers/${id}/products`)).data;
-		return response;
-	} catch (error) {}
-}
-
-export async function RequestDestinations() {
-	try {
-		const response = await api.get("/destinations");
-		const data: IPlanetDestination[] = response.data;
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
-}
-
-export async function RequestSomeMiniatures() {
-	try {
-		const storage = localStorage.getItem("cart");
-		if (storage) {
-			const cart: ICart = JSON.parse(storage);
-			const request = await api.post("/products/miniatures", { products: cart.uniques });
-			const data = request.data;
-			return data;
+export class RequestProducts extends Requests {
+	static async All() {
+		try {
+			const request = await api.get("/products/miniatures");
+			const response: IPaginateResponse = request.data;
+			const { data, total, current_page, last_page, next_page_url } = response;
+			return { data, total, current_page, last_page, next_page_url };
+		} catch (error) {
+			if (isAxiosError(error)) throw new Error(error.message);
+			else throw new Error("Unexpected Error.");
 		}
-	} catch (error) {
-		console.log(error);
+	}
+
+	static async Query(params: string, query: ProductQuery = ProductQuery.Name) {
+		try {
+			const request = await api.get(`/products/search?${query}[lk]=${params}`);
+			const response: IPaginateResponse = request.data;
+			const { data, total, current_page, last_page, last_page_url, next_page_url } = response;
+			return { data, total, current_page, last_page, last_page_url, next_page_url };
+		} catch (error) {
+			if (isAxiosError(error)) throw new Error(error.message);
+			else throw new Error("Unexpected Error.");
+		}
+	}
+
+	static async One(id: string) {
+		try {
+			const request = await api.get(`/products/${id}`);
+			const response: IProduct = request.data;
+			return response;
+		} catch (error) {
+			if (!isAxiosError(error)) throw new UnexpectedError();
+			throw new ApiRequestError(error.response?.data);
+		}
+	}
+
+	static async RequestDestinations() {
+		try {
+			const request = await api.get("/destinations");
+			const response: IPlanetDestination[] = request.data;
+			return response;
+		} catch (error) {
+			if (!isAxiosError(error)) throw new UnexpectedError();
+			throw new ApiRequestError(error.response?.data);
+		}
+	}
+
+	static async Miniatures() {
+		try {
+			const storage = localStorage.getItem("cart");
+			if (storage) {
+				const cart: ICart = JSON.parse(storage);
+				const request = await api.post("/products/miniatures", { products: cart.uniques });
+				const data = request.data;
+				return data;
+			}
+		} catch (error) {
+			if (!isAxiosError(error)) throw new UnexpectedError();
+			throw new ApiRequestError(error.response?.data);
+		}
+	}
+
+	static async FromManufacturer(query: string) {
+		try {
+			const request = await api.get(`/manufacturers/products/${query}`);
+			const response: IPaginateResponse = request.data;
+			const { data, total, current_page, last_page, last_page_url, next_page_url } = response;
+			return { data, total, current_page, last_page, last_page_url, next_page_url };
+		} catch (error) {
+			if (!isAxiosError(error)) throw new UnexpectedError();
+			throw new ApiRequestError(error.response?.data);
+		}
 	}
 }
---
+++ src/scripts/requests/Requests.ts
@@ -0,0 +1,20 @@
+import { isAxiosError } from "axios";
+import Api from "./axios-instances/PublicAxiosInstance";
+import IPaginateResponse from "@/interfaces/IPaginateResponse";
+import ApiRequestError from "../error-handling/APIRequestError";
+import UnexpectedError from "../error-handling/UnexpectedError";
+
+export default class Requests {
+	static async Paginate(next_page: string | null) {
+		try {
+			if (next_page == null) return null;
+			const request = await Api.get(next_page);
+			const response: IPaginateResponse = request.data;
+			const { data, current_page, next_page_url, total, last_page_url, last_page } = response;
+			return { data, current_page, next_page_url, total, last_page_url, last_page };
+		} catch (error) {
+			if (!isAxiosError(error)) throw new UnexpectedError();
+			throw new ApiRequestError(error.response?.data);
+		}
+	}
+}
