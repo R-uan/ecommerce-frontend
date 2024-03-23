a1c4b8c4927550dcdf5715a8e479b63401ea9667
Ruan <ruan.oliveiralopes@hotmail.com>
2024-03-23 06:10:47 -0300

    fix build error

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,10 +1,6 @@
 import { store } from "@/redux/store";
-import { Metadata } from "next";
 import { Provider } from "react-redux";
 import SignupForm from "./RegisterForm";
-export const metadata: Metadata = {
-	title: "Register to Gemini",
-};
 
 export default function Singup() {
 	return (
