e394691a73a696ab8d4e2894cfb57a5276d2d721
Ruan <ruan.oliveiralopes@hotmail.com>
2024-03-29 00:24:02 -0300

    header search bar

---
+++ src/app/components/Header/components/Search.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useRouter } from "next/navigation";
+import s from "../styles/search.module.scss";
+
+export default function Search({ transparent }: { transparent: boolean }) {
+	const router = useRouter();
+	async function HandleSearchSubmit(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+		const input = event.target as HTMLFormElement;
+		const value = (input.elements.namedItem("name") as HTMLInputElement).value;
+		const format = value.split(" ").join("_");
+		router.push(`/catalog?q=product&name=${format}`);
+	}
+
+	return (
+		<div className={s.search_bar}>
+			<form method="get" onSubmit={HandleSearchSubmit}>
+				<input
+					style={{ backgroundColor: transparent ? "transparent" : "#050A0E", color: "white" }}
+					name="name"
+					type="text"
+					placeholder="Search"
+				/>
+			</form>
+		</div>
+	);
+}
